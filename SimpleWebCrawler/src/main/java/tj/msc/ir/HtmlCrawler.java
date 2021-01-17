8e1bd216597e3d1fa15ac9e3abdabf9316612208
thilina <92thilina@gmail.com>
2021-01-17 14:14:26 +0530

    minor changes

--- SimpleWebCrawler/src/main/java/tj/msc/ir/HtmlCrawler.java
+++ SimpleWebCrawler/src/main/java/tj/msc/ir/HtmlCrawler.java
@@ -4,7 +4,6 @@ import edu.uci.ics.crawler4j.crawler.Page;
 import edu.uci.ics.crawler4j.crawler.WebCrawler;
 import edu.uci.ics.crawler4j.parser.HtmlParseData;
 import edu.uci.ics.crawler4j.url.WebURL;
-import org.w3c.dom.Document;
 
 import java.util.ArrayList;
 import java.util.Set;
